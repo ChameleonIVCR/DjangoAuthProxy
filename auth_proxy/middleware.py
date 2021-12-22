cad30ee76e14c20f8bd9e5c958e412fee385d5c7
ChameIV <chamycr@hotmail.com>
2021-12-22 17:04:05 -0500

    Correct middleware to use UUID attribute, release 1.2.3

--- auth_proxy/middleware.py
+++ auth_proxy/middleware.py
@@ -5,13 +5,13 @@ class GenerateUserProxy:
     def process_request(self, request):
         if request.user.is_authenticated() and not UserProxy.objects.filter(user_id=request.user).exists():
             UserProxy(
-                user_id=request.user
+                user_id=request.user.uuid
             ).save(
             )
 
 
 class AddUserProxyToRequest:
     def process_request(self, request):
-        user_proxy = UserProxy.objects.filter(user_id=request.user)
+        user_proxy = UserProxy.objects.filter(user_id=request.user.uuid)
         if request.user.is_authenticated() and user_proxy.exists():
             request.user_proxy = user_proxy.first()
